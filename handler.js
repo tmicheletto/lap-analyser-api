7227b1f80cdeaac68bc6e7b3f4241ce4766b6625
Tim <tim.micheletto@gmail.com>
2016-10-19 22:26:25 +1100

    Initial commit

---
+++ handler.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const sessionsCreate = require('./sessions-create.js');
+const sessionsReadAll = require('./sessions-read-all.js');
+const sessionsReadOne = require('./sessions-read-one.js');
+const sessionsUpdate = require('./sessions-update.js');
+const sessionsDelete = require('./sessions-delete.js');
+
+module.exports.create = (event, context, callback) => {
+  sessionsCreate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readAll = (event, context, callback) => {
+  sessionsReadAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readOne = (event, context, callback) => {
+  sessionsReadOne(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  sessionsUpdate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  sessionsDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
